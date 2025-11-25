0c80fd52f3fd83f39ed1fc71bd976e3c5ff1e5d9
alexsf2000 <96344400+alexsf2000@users.noreply.github.com>
2025-11-25 18:19:32 -0300

    desafio 1 concluído com exito

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Hora do Desafio';
+titulo.textContent = 'Hora do Desafio';
+// Ou, para adicionar estilo HTML:
+// titulo.innerHTML = '<span style="color:red;">Hora do Desafio</span>';
 
-function verificarChute() {
+
+function verificarConsole() {
     console.log('O botão foi clicado!');
 }
 
-
-function novoJogo() {
+function exibirAlertajs() {
     alert('Eu amo javascript!');
 }
 
+function coletarNome() {
+    let cidade = prompt('Digite o nome de uma cidade do Brasil:');
+    alert(`Estive em ${cidade} e lembrei de você.`);
+}
+
+
+function exibirSoma() {
+    let valor1 = Number(prompt('Digite o primeiro número:'));
+    let valor2 = Number(prompt('Digite o segundo número:'));
+
+    if (isNaN(valor1) || isNaN(valor2)) {
+        alert('Você precisa digitar números válidos!');
+        return;
+    }
+
+    let resultado = valor1 + valor2;
+    alert(`A soma dos valores é: ${resultado}`);
+}
