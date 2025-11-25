935e7d7d2aaf77379213c597919a6dfafe64ea2e
alexsf2000 <96344400+alexsf2000@users.noreply.github.com>
2025-11-25 16:19:32 -0300

    atualizacao do desafio js

---
+++ app.js
@@ -0,0 +1,12 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+function verificarChute() {
+    console.log('O botão foi clicado!');
+}
+
+
+function novoJogo() {
+    alert('Eu amo javascript!');
+}
+
